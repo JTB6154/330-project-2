ad512db78852037f4d7ff8c7752ef755813ddfef
JTB6154 <jtb6154@rit.edu>
2020-10-19 13:24:54 -0400

    Copy over from web audio hw

---
+++ src/audio.js
@@ -0,0 +1,80 @@
+// 1 - our WebAudio context, **we will export and make this public at the bottom of the file**
+let audioCTX;
+
+// **These are "private" properties - these will NOT be visible outside of this module (i.e. file)**
+// 2 - WebAudio nodes that are part of our WebAudio audio routing graph
+let _element, _sourceNode, analyserNode, _gainNode;
+
+// 3 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+    gain        :       .5,
+    numSamples  :       256
+})
+
+// 4 - create a new array of 8-bit integers (0-255)
+// this is a typed array to hold the audio frequency data
+let audioData = new Uint8Array(DEFAULTS.numSamples/2);
+
+
+// **Next are "public" methods - we are going to export all of these at the bottom of this file**
+function setupWebAudio(filePath){
+    // 1 - The || is because WebAudio has not been standardized across browsers yet
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    audioCTX = new AudioContext();
+    // 2 - this creates an <audio> element
+    _element = new Audio();
+
+    // 3 - have it point at a sound file
+    loadSoundFile(filePath);
+
+    // 4 - create an a source node that points at the <audio> element
+    _sourceNode = audioCTX.createMediaElementSource(_element);
+
+    // 5 - create an analyser node
+    // note the UK spelling of "Analyser"
+    analyserNode = audioCTX.createAnalyser();
+    /*
+    // 6
+    We will request DEFAULTS.numSamples number of samples or "bins" spaced equally 
+    across the sound spectrum.
+
+    If DEFAULTS.numSamples (fftSize) is 256, then the first bin is 0 Hz, the second is 172 Hz, 
+    the third is 344Hz, and so on. Each bin contains a number between 0-255 representing 
+    the amplitude of that frequency.
+    */ 
+
+    // fft stands for Fast Fourier Transform
+    analyserNode.fftSize = DEFAULTS.numSamples;
+
+    // 7 - create a gain (volume) node
+    _gainNode = audioCTX.createGain();
+    _gainNode.gain.value = DEFAULTS.gain;
+
+
+    // 8 - connect the nodes - we now have an audio graph
+    _sourceNode.connect(analyserNode);
+    analyserNode.connect(_gainNode);
+    _gainNode.connect(audioCTX.destination);
+}
+
+function loadSoundFile(filePath)
+{
+    _element.src = filePath;
+}
+
+function playCurrentSound()
+{
+    _element.play();
+}
+
+function pauseCurrentSound()
+{
+    _element.pause();
+}
+
+function setVolume(value){
+    value = Number(value);// make sure that it's a Number rather than a String
+    _gainNode.gain.value = value;
+}
+
+export{audioCTX,setupWebAudio,playCurrentSound,pauseCurrentSound,loadSoundFile,setVolume,analyserNode};
---
+++ src/main.js
@@ -0,0 +1,140 @@
+/*
+	main.js is primarily responsible for hooking up the UI to the rest of the application 
+	and setting up the main event loop
+*/
+
+// We will write the functions in this file in the traditional ES5 way
+// In this instance, we feel the code is more readable if written this way
+// If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
+
+import * as utils from './utils.js';
+import * as audio from './audio.js';
+import * as canvas from './canvas.js';
+
+const drawParams = {
+  showGradient  :true,
+  showBars      :true,
+  showCircles   :true,
+  showNoise     :false,
+  showInvert    :false,
+  showEmboss    :false
+}
+
+// 1 - here we are faking an enumeration
+const DEFAULTS = Object.freeze({
+	sound1  :  "media/New Adventure Theme.mp3"
+});
+
+function init(){
+  audio.setupWebAudio(DEFAULTS.sound1)
+	console.log("init called");
+	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
+	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+  setupUI(canvasElement);
+  canvas.setupCanvas(canvasElement,audio.analyserNode);
+  loop();
+}
+
+function setupUI(canvasElement){
+  // A - hookup fullscreen button
+  const fsButton = document.querySelector("#fsButton");
+	
+  // add .onclick event to button
+  fsButton.onclick = e => {
+    console.log("init called");
+    utils.goFullscreen(canvasElement);
+  };
+
+
+  const playbutton = document.querySelector("#playButton")
+
+  playbutton.onclick = e =>{
+    console.log(`audioCTX.sate before = ${audio.audioCTX.state}`);
+    if(audio.audioCTX.state == "suspended"){ audio.audioCTX.resume(); }
+
+    console.log(`audioCTX.sate after = ${audio.audioCTX.state}`);
+    
+
+    if(e.target.dataset.playing == "no")
+    {
+      audio.playCurrentSound();
+      e.target.dataset.playing= "yes";
+    }else{
+      audio.pauseCurrentSound();
+      e.target.dataset.playing = "no";
+    }
+  };
+
+  //get the slider and label
+  const volumeSlider = document.querySelector("#volumeSlider");
+  const volumeLabel = document.querySelector("#volumeLabel");
+
+  //add oninput to the slider
+  volumeSlider.oninput = e => {
+    //set the gain
+    audio.setVolume(e.target.value);
+    //update the label
+    volumeLabel.innerHTML = Math.round((e.target.value/2*100));
+  };
+
+  volumeSlider.dispatchEvent(new Event("input"));
+
+  //D - hookup track <select>
+  let trackSelect = document.querySelector("#trackSelect");
+
+  trackSelect.onchange = e => {
+    audio.loadSoundFile(e.target.value)
+    
+    if(playbutton.dataset.playing = "yes")
+    {
+      playbutton.dispatchEvent(new MouseEvent("click"));
+    }
+  };
+
+  const gradientCB = document.querySelector("#gradientCB");
+  gradientCB.checked = drawParams.showGradient;
+  gradientCB.onchange = e =>{
+    drawParams.showGradient = e.target.checked;
+  };
+
+  const barsCB = document.querySelector("#barsCB");
+  barsCB.checked = drawParams.showBars;
+  barsCB.onchange = e =>{
+    drawParams.showBars = e.target.checked;
+  };
+
+  const circlesCB = document.querySelector("#circlesCB");
+  circlesCB.checked = drawParams.showCircles;
+  circlesCB.onchange = e =>{
+    drawParams.showCircles = e.target.checked;
+  };
+
+  const noiseCB = document.querySelector("#noiseCB");
+  noiseCB.checked = drawParams.showNoise;
+  noiseCB.onchange = e =>{
+    drawParams.showNoise = e.target.checked;
+  }
+
+  const invertCB = document.querySelector("#invertCB");
+  invertCB.checked = drawParams.showInvert;
+  invertCB.onchange = e =>{
+    drawParams.showInvert = e.target.checked;
+  }
+  
+  const embossCB = document.querySelector("#embossCB");
+  embossCB.checked = drawParams.showEmboss;
+  embossCB.onchange = e =>{
+    drawParams.showEmboss = e.target.checked;
+  }
+  
+	
+} // end setupUI
+
+function loop(){
+  /* NOTE: This is temporary testing code that we will delete in Part II */
+    requestAnimationFrame(loop);
+
+    canvas.draw(drawParams);
+  }
+
+export {init};
